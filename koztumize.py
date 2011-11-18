1871c26cc32e5c1a393737d4134dbc34d126809b
Aymeric Bois <aymeric.bois@kozea.fr>
2011-11-18 17:59:57 +0100

    It's 18:00 and it works !

--- koztumize.py
+++ koztumize.py
@@ -2,11 +2,12 @@
 
 from __future__ import with_statement
 from flask import Flask, request, redirect, url_for, render_template, flash
+from docutils.writers.html4css1 import Writer
+import docutils.core
 import os
 
 
 app = Flask(__name__)
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
 @app.route('/')
@@ -17,8 +18,21 @@ def index():
         models[categ] = []
         for model in os.listdir('static/model/' + categ):
             models[categ].append(model)
-
     return render_template('index.html', models=models)
+
+
+@app.route('/edit/<category>/<filename>')
+def edit(category, filename):
+    return rest_to_html(category, filename)
+
+
+def rest_to_html(category, filename):
+    parts = docutils.core.publish_parts(
+        source=open(os.path.join(
+            'static', 'model', category, filename + '.rst')).read(),
+        writer=Writer())
+    text = parts['html_body']
+    return text
 
 
 if __name__ == '__main__':
