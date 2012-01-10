3b665996c1af3ddb99244a60961b5dc325321622
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2012-01-10 13:13:12 +0100

    Clean the pylint configuration

--- test/config_test.py
+++ test/config_test.py
@@ -6,6 +6,8 @@ The default config file of kuztumize.
 import os
 
 
+# This class is a monkey, it does not use its arguments
+# pylint: disable=W0613,R0201
 class FakeLDAP(object):
     """Redefine the LDAP for the test suite."""
     def search_s(self, *args, **kwargs):
@@ -14,6 +16,7 @@ class FakeLDAP(object):
 
     def simple_bind_s(self, *args, **kwargs):
         """Redefine method bind for the fake LDAP."""
+# pylint: enable=W0613,R0201
 
 LDAP = FakeLDAP()
 LDAP_PATH = None
