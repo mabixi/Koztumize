f519a5aee953b98486182204440405b48472aa38
Jérémy James <jeremy.james@kozea.fr>
2012-01-12 11:11:05 +0100

    Update config files

--- config_default.py
+++ config_default.py
@@ -24,6 +24,7 @@ LDAP_HOST = "ldap.keleos.fr"
 LDAP_PATH = "ou=People,dc=keleos,dc=fr"
 DOMAIN = 'kozea'
 ARCHIVE = os.path.join(os.path.expanduser('~/archive'))
+MODELS = os.path.join(os.path.expanduser('~/models'))
 SQLALCHEMY_ECHO = True
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://koztumize:\
 koztumize@macaron/koztumize'
